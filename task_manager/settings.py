82aa57946b8c67fc729c1a75fb9624bd395967e0
Angekok <angekok@mail.ru>
2021-10-09 19:38:50 +0300

    add minimal head too index

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%=z39r-jaw1-3$_2@!r-b!)@w%$$(ry!%f9qsl4m*%rlsasrgb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager'
+    'task_manager',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as tr
 
 
 def index(request):
     return render(request, 'index.html', context={
-        'who': 'World',
+        'tags': tr('List of tags'),
+        'users': tr('List of users'),
+        'logout': tr('Log out')
     })
