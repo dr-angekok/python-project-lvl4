59cce208e59d3a476fe87279d60fdb36f074c8fd
Angekok <angekok@mail.ru>
2021-11-02 20:40:25 +0300

    Add tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -14,3 +15,29 @@ class TaskStatus(models.Model):
     def get_absolute_url(self):
         return reverse('task_status', args=[str(self.id)])
 
+class Task(models.Model):
+    name = models.CharField(max_length=255, help_text="Task name.")
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(
+        max_length=600,
+        help_text="Enter a task")
+    status = models.ForeignKey(
+        TaskStatus,
+        on_delete=models.CASCADE,
+        null=False, blank=False)
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True, blank=True)
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="assigned_to")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('task', args=[str(self.id)])
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from tasks.models import TaskStatus
+from tasks.models import Task, TaskStatus
 
 
 class StatusesView(generic.ListView):
@@ -41,3 +42,42 @@ class StatusUpdate(LoginRequiredMixin, UpdateView):
 class StatusDelete(LoginRequiredMixin, DeleteView):
     model = TaskStatus
     success_url = reverse_lazy('statuses')
+
+
+class TasksView(generic.ListView):
+    model = Task
+    template_name = 'tasks/tasks_list.html'
+    context_object_name = 'tasks_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class TaskView(generic.DetailView):
+    model = Task
+    template_name = "tasks/task.html"
+    success_url = reverse_lazy('tasks')
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+    def get_initial(self, *args, **kwargs):
+        initial = super(TaskCreate, self).get_initial(**kwargs)
+        initial['creator'] = self.request.user
+        initial['status'] = get_object_or_404(TaskStatus, name="New")
+        return initial
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
