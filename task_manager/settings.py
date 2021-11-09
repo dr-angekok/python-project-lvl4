e20f86d627bc9c9235225ecf8ae88c2cb44600c9
Angekok <angekok@mail.ru>
2021-11-09 19:25:03 +0300

    Add localhost to settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0', 'localhost']
 
 
 # Application definition
