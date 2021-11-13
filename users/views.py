9f12f96c7791a1c647077684eb14efb8bf501d15
Angekok <angekok@mail.ru>
2021-11-13 12:59:28 +0300

    Translate fix.

--- users/views.py
+++ users/views.py
@@ -7,51 +7,54 @@ from django.views import View
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as trans
+
 
 
 class Create(View, SuccessMessageMixin):
-    template_name = "users/create.html"
-    success_message = 'Успешно.'
+    template_name = 'users/create.html'
+    success_message = trans('Success.')
 
     def get(self, request):
-        return render(request, self.template_name, context={"form": UserCreationForm()})
+        return render(request, self.template_name, context={'form': UserCreationForm()})
 
     def post(self, request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect("login")
+            return redirect('login')
         else:
-            return render(request, self.template_name, context={"form": form})
+            return render(request, self.template_name, context={'form': form})
 
 
 class Update(LoginRequiredMixin, UpdateView, SuccessMessageMixin):
     model = User
     fields = ('username', 'first_name', 'last_name', 'password')
     template_name = 'users/update.html'
-    success_url = "/users/"
-    success_message = 'Успешно отредактирован.'
+    success_url = '/users/'
+    success_message = 'Successfully edited.'
 
 
 class Delete(LoginRequiredMixin, View):
-    login_url = "login"
-    template_name = "users/delete.html"
+    login_url = 'login'
+    template_name = 'users/delete.html'
+    success_message = trans('Successfully delete.')
 
     def get(self, request, user_id):
-        context = {"user": User.objects.get(id=user_id)}
+        context = {'user': User.objects.get(id=user_id)}
         return render(request, self.template_name, context=context)
 
     def post(self, request, user_id):
         user = User.objects.get(id=user_id)
         user.delete()
-        return redirect("/users/")
+        return redirect('/users/')
 
 
 class List(View):
-    template_name = "users/list.html"
+    template_name = 'users/list.html'
 
     def get(self, request):
         users = User.objects.all()
-        context = {"users": users}
+        context = {'users': users}
         return render(request, self.template_name, context=context)
 
