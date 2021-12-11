16e147f6a067327ece303810f7b21288fe31fdc3
Angekok <angekok@mail.ru>
2021-12-11 12:26:18 +0300

    Added extended labels tests.

--- labels/tests.py
+++ labels/tests.py
@@ -5,6 +5,18 @@ from labels import views
 from labels.models import TaskLabel
 
 
+TEST_CASE = (
+    (('/labels/', '/login/',
+      {'username': 'Igor', 'password': 'password!@#$123'}), ('Any label',)),
+    (('/labels/1/', None,
+      None), ('Any label')),
+    (('/labels/', '/labels/1/update/',
+      {'name': 'Changed label', }), ('Changed label', '"success">Метка успешно изменена<')),
+    (('/labels/', '/labels/1/delete/',
+      None), ('"success">Метка успешно удалена<', 'У вас нет меток'))
+)
+
+
 class LabelTest(TestCase):
 
     def setUp(self):
@@ -19,6 +31,12 @@ class LabelTest(TestCase):
         label = TaskLabel.objects.create(name='Any label')
         return label
 
+    def get_page(self, get_link, post_link=None, post_data=None):
+        if post_link:
+            self.client.post(post_link, post_data)
+        response = self.client.get(get_link)
+        return response.content.decode("utf-8")
+
     def test_label_create(self):
         label = self.createLabel()
         self.assertTrue(isinstance(label, TaskLabel))
@@ -30,7 +48,9 @@ class LabelTest(TestCase):
         request.user = self.user
         response = views.LabelsView.as_view()(request)
         self.assertEqual(response.status_code, 200)
-        response = self.client.post('/login/', {'username': 'Igor', 'password': 'password!@#$123'})
-        response = self.client.get('/labels/')
-        content = response.content.decode("utf-8")
-        self.assertTrue(content.__contains__('Any label'))
+
+        for prepear_data, cases in TEST_CASE:
+            get_link, post_link, post_data = prepear_data
+            content = self.get_page(get_link, post_link, post_data)
+            for case in cases:
+                self.assertTrue(content.__contains__(case))
