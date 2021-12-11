bd3f2c5c427eb77b1d54e25ef2647d3ef5f7fd12
Angekok <angekok@mail.ru>
2021-12-11 12:01:27 +0300

    Added extended status tests

--- statuses/tests.py
+++ statuses/tests.py
@@ -5,6 +5,18 @@ from statuses import views
 from statuses.models import TaskStatus
 
 
+TEST_CASE = (
+    (('/statuses/', '/login/',
+      {'username': 'Igor', 'password': 'password!@#$123'}), ('Any status',)),
+    (('/statuses/1/', None,
+      None), ('Any status')),
+    (('/statuses/', '/statuses/1/update/',
+      {'name': 'Changed status', }), ('Changed status', '"success">Статус успешно изменён<')),
+    (('/statuses/', '/statuses/1/delete/',
+      None), ('"success">Статус успешно удалён<', 'У вас нет статусов'))
+)
+
+
 class SratusTest(TestCase):
 
     def setUp(self):
@@ -24,27 +36,21 @@ class SratusTest(TestCase):
         self.assertTrue(isinstance(status, TaskStatus))
         self.assertEqual(TaskStatus.objects.count(), 1)
 
+    def get_page(self, get_link, post_link=None, post_data=None):
+        if post_link:
+            self.client.post(post_link, post_data)
+        response = self.client.get(get_link)
+        return response.content.decode("utf-8")
+
     def test_status_view(self):
         self.createStatus()
         request = self.factory.get('/statuses/')
         request.user = self.user
         response = views.StatusesView.as_view()(request)
         self.assertEqual(response.status_code, 200)
-        response = self.client.post('/login/', {'username': 'Igor', 'password': 'password!@#$123'})
-        response = self.client.get('/statuses/')
-        content = response.content.decode("utf-8")
-        self.assertTrue(content.__contains__('Any status'))
-        response = self.client.get('/statuses/1/')
-        content = response.content.decode("utf-8")
-        self.assertTrue(content.__contains__('Any status'))
-        response = self.client.post('/statuses/1/update/', {'name': 'Changed status', })
-        response = self.client.get('/statuses/')
-        content = response.content.decode("utf-8")
-        self.assertTrue(content.__contains__('Changed status'))
-        self.assertTrue(content.__contains__('"success">Статус успешно изменён<'))
-        response = self.client.post('/statuses/1/delete/')
-        response = self.client.get('/statuses/')
-        content = response.content.decode("utf-8")
-        self.assertFalse(content.__contains__('Changed status'))
-        self.assertTrue(content.__contains__('"success">Статус успешно удалён<'))
-        self.assertTrue(content.__contains__('У вас нет статусов'))
+
+        for prepear_data, cases in TEST_CASE:
+            get_link, post_link, post_data = prepear_data
+            content = self.get_page(get_link, post_link, post_data)
+            for case in cases:
+                self.assertTrue(content.__contains__(case))
